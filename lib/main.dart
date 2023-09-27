366389bc1398785cd53704e8dddf63fac9644d6e
Zannatul Naim <zannatulnaim09@gmail.com>
2023-09-27 21:35:43 +0600

    main file updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,20 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-void main() {
-  runApp(MyApp());
-}
+import 'package:flutter_calculator/CalculatorHomePage.dart';
+void main() => runApp(
+  const MyApp()
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-   @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    // ↓ Add this.
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        // ↓ Change this line.
-        child: Text(pair.asLowerCase, style: style),
-      ),
-    );
-  }
-
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-}
-
-class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
-
-          // ↓ Add this.
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('Next'),
-          ),
-        ],
+    return MaterialApp (
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const CalculatorHomePage(title: 'Calculator'),
     );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(pair.asLowerCase);
   }
 }
